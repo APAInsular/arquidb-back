e5a3f56ebf577936eacbc198c94882fb23fd4133
Fabio Acosta <fabioacosta00001@gmail.com>
2025-03-26 13:16:29 +0000

    Permissions adjusted

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -17,18 +17,76 @@ class RolesAndPermissionsSeeder extends Seeder
         $user = Role::create(['name' => 'user']);
 
         // 2. Create permissions
-        $get = Permission::create(['name' => 'GET ALL']);
-        $insert = Permission::create(['name' => 'INSERT ALL']);
-        $update = Permission::create(['name' => 'UPDATE ALL']);
-        $delete = Permission::create(['name' => 'DELETE ALL']);
-
         $permissions = [
-            'get persons'
+            //people
+            'get people',
+            'insert people',
+            'update people',
+            'delete people',
+            //collegiates
+            'get collegiates',
+            'insert collegiates',
+            'update collegiates',
+            'delete collegiates',
+            //clients
+            'get clients',
+            'insert clients',
+            'update clients',
+            'delete clients',
+            //expedients
+            'get expedients',
+            'insert expedients',
+            'update expedients',
+            'delete expedients',
+            //phases
+            'get phases',
+            'insert phases',
+            'update phases',
+            'delete phases',
+            //documents
+            'get documents',
+            'insert documents',
+            'update documents',
+            'delete documents',
+            'sign documents',
+            //records
+            'get records',
+            'insert records',
+            'update records',
+            'delete records',
+            //phones
+            'get phones',
+            'insert phones',
+            'update phones',
+            'delete phones',
+            //addresses
+            'get addresses',
+            'insert addresses',
+            'update addresses',
+            'delete addresses',
+            //emails
+            'get emails',
+            'insert emails',
+            'update emails',
+            'delete emails',
+            //expedient_person
+            'get expedient_person',
+            'insert expedient_person',
+            'update expedient_person',
+            'delete expedient_person'
         ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
+        $getPermissions = array_filter($permissions, function ($permission) {
+            return strpos($permission, 'get ') === 0; // Verifica si el string comienza con 'get '
+        });
 
         // 3. Assign permission to a role
         $superAdmin->givePermissionTo(Permission::all());
         $visor->givePermissionTo(Permission::all());
-        $user->givePermissionTo($get);
+        $user->syncPermissions($getPermissions);
     }
 }
