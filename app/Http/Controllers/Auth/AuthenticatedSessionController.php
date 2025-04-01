946f7cba5991964efa3f114fdf21233e1673fee9
Alexloven <reyvaldiviaalejandro@gmail.com>
2025-04-01 13:40:22 +0200

    Create: creamos la autenticacion con el bear token

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
 class AuthenticatedSessionController extends Controller
@@ -13,26 +13,35 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request): JsonResponse
     {
-        $request->authenticate();
 
-        $request->session()->regenerate();
+    // Autenticar al usuario
+    $request->authenticate();
 
-        return response()->noContent();
+    // Crear el token de autenticación
+    $user = Auth::user();
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    // Retornar el token como respuesta JSON
+    return response()->json([
+        'token' => $token,
+        'user' => $user
+    ]);
     }
 
     /**
      * Destroy an authenticated session.
      */
-    public function destroy(Request $request): Response
+    public function destroy(Request $request): JsonResponse
     {
+
+        $user = Auth::user();
+        $user->tokens()->delete();
         Auth::guard('web')->logout();
-
         $request->session()->invalidate();
-
         $request->session()->regenerateToken();
 
-        return response()->noContent();
+        return response()->json(['message' => 'Successfully logged out']);
     }
 }
