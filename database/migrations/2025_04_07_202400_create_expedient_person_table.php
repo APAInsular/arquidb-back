7f98b78589a26a8392c0bdce7bcfb41fc52cc55a
Fabio Acosta <fabioacosta00001@gmail.com>
2025-04-07 20:34:52 +0000

    Center added through blueprint

--- database/migrations/2025_04_07_202400_create_expedient_person_table.php
+++ database/migrations/2025_04_07_202400_create_expedient_person_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('expedient_person', function (Blueprint $table) {
             $table->foreignId('expedient_id');
             $table->foreignId('person_id');
-            $table->timestamps();
         });
 
         Schema::enableForeignKeyConstraints();
