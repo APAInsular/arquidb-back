a523745e0d178a3c611a417fc861e42ecef303c4
Fabio Acosta <fabioacosta00001@gmail.com>
2025-04-25 10:15:51 +0000

    MultiImport created and configurated

--- app/Imports/ClientsImport.php
+++ app/Imports/ClientsImport.php
@@ -8,14 +8,14 @@ use Maatwebsite\Excel\Concerns\ToModel;
 class ClientsImport implements ToModel
 {
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
         return new Client([
-            //
+            'person_id' => '',
         ]);
     }
 }
--- app/Imports/MultiSheetImport.php
+++ app/Imports/MultiSheetImport.php
@@ -19,16 +19,20 @@ class MultiSheetImport implements WithMultipleSheets, WithEvents
 
         return [
             // Aquí defines qué importador corresponde a cada hoja
-            // '' => new PeopleImport(),
+            // '' => new PeopleImport($this),
             // '' => new CollegiatesImport(),
             // 'tblclientes' => [
-            // new PeopleImport(),
+            // new PeopleImport($this),
             // new ClientsImport(),
             // new PhonesImport($this, 21),
             // new AddressesImport(),
             // new EmailsImport(),
             // ],
-            'tblclientes' => new PeopleImport($this),
+            // 'tblclientes' => new PeopleImport($this),
+            'tblclientes' => new MultiImport([
+                new PeopleImport($this),
+                new PhonesImport($this, 21),
+            ], $this),
             // 'TBLEXPEDIENTES' => new ExpedientsImport(),
             // 'TBLEXPEDIENTES_FASES' => new PhasesImport(),
             // '' => new DocumentsImport(),
