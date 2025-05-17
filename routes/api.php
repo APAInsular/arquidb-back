c55f64364e99dd75e62925c68b7326bf79fdb44f
Alexloven <reyvaldiviaalejandro@gmail.com>
2025-05-17 21:15:32 +0200

    añadimos que puedas editar el nombre y gmail del usuario ademas de poder borrar el user

--- routes/api.php
+++ routes/api.php
@@ -20,20 +20,23 @@ use App\Http\Controllers\Api\v1\PhaseDocumentsController;
 use App\Http\Controllers\Api\v1\PhoneController;
 use App\Http\Controllers\Api\v1\RecordController;
 use App\Http\Controllers\Api\v1\UserController;
+use App\Http\Controllers\UserController as AuthUserController;
 use App\Http\Controllers\Api\v1\UserDocumentsController;
 use App\Http\Controllers\Api\v1\UserRecordsController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Orion\Facades\Orion;
 use App\Http\Controllers\ExcelImportController;
 use App\Http\Controllers\FileController;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthUserController::class, 'show']);
+    Route::put('/user', [AuthUserController::class, 'update']);
+    Route::delete('/user', [AuthUserController::class, 'destroy']);
 });
+
 
 Route::post('login', [AuthenticatedSessionController::class, 'store']);
 Route::post('/forgot-password', [PasswordResetLinkController::class, 'store']);
