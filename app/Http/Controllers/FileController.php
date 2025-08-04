ed3bf9c18304998b15232a56f8425466984b7c8b
Fabio Acosta <fabioacosta00001@gmail.com>
2025-08-04 14:30:19 +0100

    addPhaseDocuments function modified

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -57,7 +57,7 @@ class FileController extends Controller
                 }
 
                 // Almacenar el archivo en S3
-                $filePath = Storage::disk('s3')->putFile($folderPath, $file);
+                $filePath = Storage::disk('s3')->putFile($folderPath, $file, 'public');
 
                 if (!$filePath) {
                     throw new \Exception('No se pudo guardar el archivo en S3: ' . $file->getClientOriginalName());
@@ -71,7 +71,7 @@ class FileController extends Controller
                 ]);
 
                 // Añadir la URL pública
-                $document->url = Storage::disk('s3')->url($filePath);
+                $document->url = Storage::disk('s3')->temporaryUrl($filePath, now()->addMinutes(10));
                 $storedDocuments[] = $document;
             }
 
