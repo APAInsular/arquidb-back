77566a90e807f60f244eb1fe3f9ce5b3f07f2a1a
Fabio Acosta <fabioacosta00001@gmail.com>
2025-08-26 12:40:05 +0100

    RecordController index configurated

--- app/Http/Controllers/Api/v1/RecordController.php
+++ app/Http/Controllers/Api/v1/RecordController.php
@@ -6,10 +6,21 @@ use App\Models\Record;
 use Orion\Concerns\DisableAuthorization;
 use Orion\Concerns\DisablePagination;
 use Orion\Http\Controllers\Controller;
+use Orion\Http\Requests\Request as OrionRequest;
 use Illuminate\Http\Request;
 
 class RecordController extends Controller
 {
     // use DisableAuthorization, DisablePagination;
     protected $model = Record::class;
+
+    public function index(OrionRequest $request)
+    {
+        $all = $request->boolean('all', false);
+
+        $query = Record::orderBy('created_at', 'desc');
+        $all ? $records = $query->get() : $records = $query->paginate(20);
+
+        return response()->json($records);
+    }
 }
